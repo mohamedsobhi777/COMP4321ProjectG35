ecc1584424cb639b3db981f2f8fb2796f184c55e
msmhmorsi <msmhmorsi@connect.ust.hk>
2024-05-10 03:25:11 +0800

    UI improvements

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,7 +18,9 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import SearchResultCard from "@/components/search-results";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import HeroSection from "@/components/hero";
+import { MoonLoader, BounceLoader } from 'react-spinners'
 
 export const dynamic = 'force-dynamic';
 
@@ -26,11 +28,13 @@ export default function Component() {
 
   const [isPending, startTransition] = useTransition();
   const [error, setError] = useState<string | undefined>();
-  const [results, setResults] = useState<any[]>([]);
+  const [results, setResults] = useState<any[] | undefined>([]);
 
   const searchParams = useSearchParams()
   const router = useRouter();
   const query = searchParams.get('query') || ""
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   const form = useForm<SearchTermType>({
     resolver: zodResolver(searchTermSchema),
@@ -39,8 +43,31 @@ export default function Component() {
     },
   });
 
-  const onSubmit = (values: SearchTermType) => {
+  function updateSearchParam({ key, value }: {
+    key: string,
+    value: string
+  }): string {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      // set the search parameter if value is not empty
+      params.set(key, value)
+    } else {
+      params.delete(key)
+    }
+    return `${pathname}?${params.toString()}`
+  }
 
+  function updateSearchParamForCurrentPage({ key, value }: {
+    key: string,
+    value: string
+  }) {
+
+    const newUrl = updateSearchParam({ key, value })
+    replace(newUrl)
+  }
+
+
+  const onSubmit = (values: SearchTermType) => {
     startTransition(() => {
       router.replace(`/?query=${values.query}`, { scroll: false });
       searchAction(values)
@@ -66,12 +93,20 @@ export default function Component() {
     }
   }, [query])
 
+
+  const resetSearch = () => {
+    updateSearchParamForCurrentPage({ key: "query", value: "" });
+    form.setValue("query", "");
+    setResults(undefined)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-[400px] px-4">
 
-      <h1 className="py-6 text-2xl">
+      {/* <h1 className="py-6 text-2xl">
         COMP4321 Search Engine
-      </h1>
+      </h1> */}
+      <HeroSection resetSearch={resetSearch} />
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)} className="max-w-2xl mx-auto w-full">
@@ -81,17 +116,28 @@ export default function Component() {
 
             <FormField
               name="query"
+              disabled={isPending}
               control={form.control}
               render={({ field }) => (
                 <FormItem className="flex-grow">
-                  {/* <FormLabel>Search </FormLabel> */}
                   <FormControl>
-                    <Input
-                      disabled={isPending}
-                      placeholder="Search..."
-                      {...field}
-                      className="block p-4 text-sm border border-gray-300 rounded-lg text-white"
-                    />
+                    <div className="rounded-full shadow-lg p-2 flex items-center">
+                      <input
+                        placeholder="Search the web"
+                        type="text"
+                        {...field}
+                        className="flex-1 bg-transparent px-4 py-2 focus:outline-none"
+                      />
+                      <Button
+                        className="rounded-full py-0 px-2"
+                      >
+                        {
+                          isPending
+                            ? <BounceLoader size={24} color="#2463EB" />
+                            : <Search className="w-5 h-5 mx-1 text-gray-500 dark:text-gray-400" />
+                        }
+                      </Button>
+                    </div>
                   </FormControl>
                   {/* <FormDescription>
                     This is the title of your artwork
@@ -100,27 +146,31 @@ export default function Component() {
                 </FormItem>
               )}
             />
-            <Button type="submit" disabled={isPending} className="text-white bg-black text-sm px-4 h-full border-2 border-black">
+            {/* <Button type="submit" disabled={isPending} className="text-white bg-black text-sm px-4 h-full border-2 border-black">
               <Search className="" />
-            </Button>
+            </Button> */}
           </div>
         </form>
       </Form>
 
-      <div className="flex flex-col py-4">
-        <div>
-          {results.length} Results
-        </div>
-        <div className="flex flex-col gap-4 w-full">
-          {results.map((result) => (
-            <SearchResultCard
-              key={result.id}
-              data={result}
-              similarResultsLink={`/?query=${query} ${(result.keywords.map((word: any) => word.wordId)).join(' ')}`}
-            />
-          ))}
-        </div>
-      </div>
+      {
+        results !== undefined && (
+          <div className="flex flex-col py-4">
+            <div>
+              {results.length} Results
+            </div>
+            <div className="flex flex-col gap-4 w-full">
+              {results.map((result) => (
+                <SearchResultCard
+                  key={result.id}
+                  data={result}
+                  similarResultsLink={`/?query=${query} ${(result.keywords.map((word: any) => word.wordId)).join(' ')}`}
+                />
+              ))}
+            </div>
+          </div>
+        )
+      }
 
     </div>
   )
---
+++ frontend/components/hero.tsx
@@ -0,0 +1,43 @@
+'use client';
+import React from 'react'
+import { Button, buttonVariants } from './ui/button'
+import Link from 'next/link'
+import { Search } from 'lucide-react'
+import { useRouter } from 'next/navigation';
+
+type Props = {
+    resetSearch: () => void;
+}
+
+const HeroSection = (props: Props) => {
+    const router = useRouter();
+    
+    return (
+        <div className="py-20 pb-10 mx-auto text-center flex flex-col items-center max-w-3xl">
+            <div
+                // href="/"
+                onClick={props.resetSearch}
+                className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                <span className="text-blue-600 text-3xl">
+                    COMP4321
+                </span>
+                <br />
+                Search Engine
+            </div>
+            <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+                By{" "}
+                <Link href="https://github.com/Azis64" target="_blank" className='underline'>
+                    Axis
+                </Link>,{" "}
+                <Link href="https://github.com/CheungHin-Hiu" target="_blank" className='underline'>
+                    CheungHin-Hui
+                </Link>, and {" "}
+                <Link href="https://github.com/mohamedsobhi777" target="_blank" className='underline'>
+                    Mohamed
+                </Link>
+            </p>
+        </div>
+    )
+}
+
+export default HeroSection
